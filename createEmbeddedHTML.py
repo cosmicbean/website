96bc2ea5487c106b23dcd2b7a264953bb9cc9f78
Alexander Kampas <alex.kampas@gmail.com>
2025-09-09 14:06:14 +0300

    added extra map stuff

---
+++ createEmbeddedHTML.py
@@ -0,0 +1,14 @@
+s1 = '''<gmp-map center="'''
+s2 = '''" zoom="4" map-id="DEMO_MAP_ID" style="height: 600px"><gmp-advanced-marker position="'''
+s3 = '''" title="Alexander's last known location"></gmp-advanced-marker></gmp-map>'''
+
+with open("locationFile.txt", "r") as f:
+    x = f.readlines()
+    res = []
+    for c in x[-1]:
+        if c in "1234567890-,.":
+            res.append(c)
+    s = "".join(res)
+    final = s1 + s + s2 + s + s3
+    print(final)
+
--- updateLocation.sh
+++ updateLocation.sh
@@ -1,7 +1,11 @@
 rm locationFile.txt
 # get current location
 curl -s https://ipinfo.io/$(curl -s https://ipinfo.io/ip) | jq '.city, .region, .country, .loc' >> locationFile.txt
+rm mapStuff.html
+cp script.html mapStuff.html
+python3 createEmbeddedHTML.py >> mapStuff.html
 # push change to git
 git add locationFile.txt
+git add mapStuff.html
 git commit -m "Alex Location Update"
 git push origin main
