c31fb8fd48e48761b34ab314fc7f7674f5aa5818
Alexander Kampas <alex.kampas@gmail.com>
2025-09-11 02:29:47 +0300

    changes to location finding, now using location services

--- createEmbeddedHTML.py
+++ createEmbeddedHTML.py
@@ -1,6 +1,10 @@
 s1 = '''<gmp-map center="'''
-s2 = '''" zoom="4" map-id="DEMO_MAP_ID" style="height: 600px"><gmp-advanced-marker position="'''
-s3 = '''" title="Alexander's last known location"></gmp-advanced-marker></gmp-map>'''
+s2 = '''" zoom="4" map-id="DEMO_MAP_ID" style="height: 500px"><gmp-advanced-marker position="'''
+s3 = '''" title="Alexander's last known location"></gmp-advanced-marker></gmp-map>\n'''
+s4 = '''<p>Last coordinates: '''
+s5 = ''', updated at '''
+s6 = '''.</p>'''
+from datetime import datetime
 
 with open("locationFile.txt", "r") as f:
     x = f.readlines()
@@ -11,6 +15,6 @@ with open("locationFile.txt", "r") as f:
         elif c == " ":
             res.append(",")
     s = "".join(res)
-    final = s1 + s + s2 + s + s3
+    final = s1 + s + s2 + s + s3 + s4 + s + s5 + datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + s6
     print(final)
 
